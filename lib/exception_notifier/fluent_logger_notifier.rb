9a13f5960a08c0393dd4c0632d4f13f8f61b5428
Shimpei Makimoto <makimoto@tsuyabu.in>
2013-12-16 10:30:33 +0900

    Implement FluentLoggerNotifier

--- lib/exception_notifier/fluent_logger_notifier.rb
+++ lib/exception_notifier/fluent_logger_notifier.rb
@@ -1,4 +1,75 @@
+require "fluent-logger"
 module ExceptionNotifier
   class FluentLoggerNotifier
+
+    attr_accessor :logger, :template
+    class ConfigurationError < Exception;end
+
+    def initialize(options)
+      @template = options.delete(:template)
+      raise ConfigurationError, "`template` key must be set" unless @template
+
+      logger_settings = options.delete(:logger_settings) || {}
+      if options.delete(:test_logger)
+        @logger = Fluent::Logger::TestLogger.new
+      else
+        tag_prefix = options.delete(:tag_prefix)
+        @logger = Fluent::Logger::FluentLogger.new(tag_prefix, logger_settings)
+      end
+    end
+
+    def call(exception, options={})
+      arg = Argument.build(template, exception, options)
+      logger.post(nil, arg)
+    end
+
+    class SetupError < Exception;end
+
+    class Argument
+      def initialize(template, exception, options = {})
+        @exception = exception
+        @options = options
+        @template = template
+      end
+
+      def build
+        expand_object(@template)
+      end
+
+      def self.build(template, exception, options = {})
+        self.new(template, exception, options).build
+      end
+
+      private
+
+      def expand_object(obj)
+        case obj
+        when Hash
+          expand_hash(obj)
+        when Array
+          expand_array(obj)
+        when Proc
+          expand_proc(obj)
+        else
+          obj
+        end
+      end
+
+      def expand_proc(prok)
+        expand_object(prok.call(@exception, @options))
+      end
+
+      def expand_array(array)
+        array.map {|element| expand_object(element) }
+      end
+
+      def expand_hash(hash)
+        {}.tap do |result|
+          hash.each do |k, v|
+            result[k] = expand_object(v)
+          end
+        end
+      end
+    end
   end
 end
--- spec/exception_notifier/fluent_logger_notifier_spec.rb
+++ spec/exception_notifier/fluent_logger_notifier_spec.rb
@@ -1,4 +1,103 @@
 require "spec_helper"
 
 describe ExceptionNotifier::FluentLoggerNotifier do
+  class FooException < Exception;end
+
+  let(:exception) do
+    FooException.new("bar message")
+  end
+
+  let(:notifier) do
+    ExceptionNotifier::FluentLoggerNotifier.new(
+      test_logger: true,
+      template: {
+        message: ->(exception, options) { "Exception: #{exception.class}: #{exception.message}" }
+      }
+    )
+  end
+
+  describe "#call" do
+    it "logs valid data" do
+      notifier.call(FooException.new("bar message"))
+      notifier.logger.queue.last.should == { message: "Exception: FooException: bar message" }
+    end
+  end
+
+  describe ExceptionNotifier::FluentLoggerNotifier::Argument do
+    subject do
+      ExceptionNotifier::FluentLoggerNotifier::Argument.build(template, exception, {})
+    end
+
+    context "with String" do
+      let(:template) do
+        "string"
+      end
+
+      it "returns string without any change" do
+        should == "string"
+      end
+    end
+
+    context "with Proc" do
+      let(:template) do
+        ->(e, opts) { "Exception: #{e.class}: #{e.message}" }
+      end
+
+      it "returns intended string" do
+        should == "Exception: FooException: bar message"
+      end
+    end
+
+    context "with Hash" do
+      let(:template) do
+        {
+          class_name: ->(e, opts) { e.class.to_s },
+          message: ->(e, opts) { e.message},
+          baz: 42,
+        }
+      end
+
+      it "returns intended hash" do
+        should == { class_name: "FooException", message: "bar message", baz: 42 }
+      end
+    end
+
+    context "with Array" do
+      let(:template) do
+        [
+          ->(e, opts) { 1 + 42 },
+          ->(e, opts) { e.class.to_s },
+          nil,
+        ]
+      end
+
+      it "returns intended array" do
+        [43, "FooException", nil]
+      end
+    end
+
+    context "with nested templates" do
+      let(:template) do
+        {
+          string: "string",
+          int: 42,
+          hash: {
+            proc: ->(e, opts) { e.class.to_s }
+          },
+          array: [42, ->(e, opts) { e.message }],
+        }
+      end
+
+      it "returns intended value" do
+        {
+          string: "string",
+          int: 42,
+          hash: {
+            proc: "FooException",
+          },
+          array: [42, "bar message"],
+        }
+      end
+    end
+  end
 end
